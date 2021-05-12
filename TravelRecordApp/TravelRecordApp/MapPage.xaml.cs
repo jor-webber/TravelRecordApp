dd5c9904c32ce2bc79e2a2b842bc0f7a0b866d2b
Jordan Webber <webber_jordan_95@hotmail.com>
2021-05-12 11:26:52 -0400

    Add project files.

---
+++ TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
@@ -0,0 +1,110 @@
+﻿using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        private bool hasLocationPermission = false;
+
+        public MapPage()
+        {
+            InitializeComponent();
+
+            GetPermissions();
+        }
+
+        private async void GetPermissions()
+        {
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync<LocationWhenInUsePermission>();
+                if (status != PermissionStatus.Granted)
+                {
+
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.LocationWhenInUse))
+                    {
+                        await DisplayAlert("Need your location", "We need to access your location", "Ok");
+                    }
+
+                    status = await CrossPermissions.Current.RequestPermissionAsync<LocationWhenInUsePermission>();
+                }
+
+                if (status == PermissionStatus.Granted)
+                {
+                    hasLocationPermission = true;
+                    locationsMap.IsShowingUser = true;
+
+                    GetLocation();
+                }
+                else
+                {
+                    await DisplayAlert("Need your location", "We need to access your location", "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+            }
+            
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (hasLocationPermission)
+            {
+                var locator = CrossGeolocator.Current;
+
+                locator.PositionChanged += Locator_PositionChanged;
+                await locator.StartListeningAsync(TimeSpan.Zero, 100);
+            }
+
+                GetLocation();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            CrossGeolocator.Current.StopListeningAsync();
+            CrossGeolocator.Current.PositionChanged -= Locator_PositionChanged;
+        }
+
+        private void Locator_PositionChanged(object sender, PositionEventArgs e)
+        {
+            MoveMap(e.Position);
+        }
+
+        private async void GetLocation()
+        {
+            if (hasLocationPermission)
+            {
+                var locator = CrossGeolocator.Current;
+                var position = await locator.GetPositionAsync();
+
+                MoveMap(position);
+            }
+        }
+
+        private void MoveMap(Plugin.Geolocator.Abstractions.Position position)
+        {
+            var center = new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude);
+
+            locationsMap.MoveToRegion(new MapSpan(center, 1, 1));
+        }
+    }
+}
